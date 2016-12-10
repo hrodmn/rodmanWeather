566a86a564b63427de79f79e5938e461757f4fdb
hrodmn <henry.rodman@gmail.com>
2016-12-10 14:31:45 -0800

    changed date format

--- weatherQuery.php
+++ weatherQuery.php
@@ -14,7 +14,7 @@ $result = $sql->query($query) or exit("Error code ({$sql->errno}): {$sql->error}
 $rows = array();
 $table = array();
 $table['cols'] = array(
-		array('label' => 'datetime', 'type' => 'string'),
+		array('label' => 'datetime', 'type' => 'date'),
 		array('label' => 'temperature', 'type' => 'number'),
 		array('label' => 'humidity', 'type' => 'number')
 );
@@ -23,7 +23,12 @@ foreach($result as $r) {
 
 	$temp = array();
 
-	$temp[] = array('v' => (string) $r['datetime']);
+	$temp[] = array('v' => 'Date('.date('Y',strtotime($r['datetime'])).',' . 
+                                     (date('n',strtotime($r['datetime'])) - 1).','.
+                                     date('d',strtotime($r['datetime'])).','.
+                                     date('H',strtotime($r['datetime'])).','.
+                                     date('i',strtotime($r['datetime'])).','.
+                                     date('s',strtotime($r['datetime'])).')');
 	$temp[] = array('v' => (float) $r['temperature']);
 	$temp[] = array('v' => (float) $r['humidity']);
 	$rows[] = array('c' => $temp);
